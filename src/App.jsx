91d51d880f35b759a0e11d095013e88cfe0da67e
bamboo-wood <64763469+bamboo-wood@users.noreply.github.com>
2022-06-20 20:31:45 +0900

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
 import React, { useEffect, useState } from "react";
 
 const App = () => {
-  return null;
+  return (
+    <React.Fragment>
+      <div>
+        <input placeholder="input todo" />
+        <button>add</button>
+      </div>
+      <div>
+        <p>incomplete todos</p>
+        <ul>
+          <li>test todo</li>
+          <button>completed</button>
+          <button>delete</button>
+
+          <li>test todo</li>
+          <button>completed</button>
+          <button>delete</button>
+        </ul>
+      </div>
+      <div>
+        <p>completed todos</p>
+        <ul>
+          <li>test todo</li>
+          <button>back</button>
+        </ul>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default App;
