b25e6808fe7e132a2f90067a43e0df6e1cdfd42d
bamboo-wood <64763469+bamboo-wood@users.noreply.github.com>
2022-06-20 20:54:58 +0900

    add feature to delete tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,46 @@
 import React, { useEffect, useState } from "react";
 
 const App = () => {
+  const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setInCompleteTodos] = useState(["aaaa", "bbbb"]);
   const [completeTodos, setCompleteTodos] = useState(["cccc"]);
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  // function to click add button
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setInCompleteTodos(newTodos);
+    setTodoText("");
+  };
+
+  // function to click delete button
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setInCompleteTodos(newTodos);
+  };
 
   return (
     <React.Fragment>
       <div className="input-area">
-        <input placeholder="input todo" />
-        <button>add</button>
+        <input
+          placeholder="input todo"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>add</button>
       </div>
       <div className="incomplete-area">
         <p className="title">incomplete todos</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
                 <button>completed</button>
-                <button>delete</button>
+                <button onClick={() => onClickDelete(index)}>delete</button>
               </div>
             );
           })}
